23c9f676bd6a14ab271326aeb644e48474e3ad4b
PivovarovAndrei <160382197+PivovarovAndrei@users.noreply.github.com>
2024-04-16 21:04:27 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,14 +1,13 @@
 // Copyright 2021 NNTU-CS
 int countPairs1(int *arr, int len, int value) {
     int count = 0;
-    for (int i = 0; i < len; i++){
-        for (int j = i + 1; j < len; j++){
-            if (arr[i] + arr[j] == value)
+    for (int i = 0; i < len - 1; i++)
+        for (int j = i + 1; j < len; j++)
+            if ((arr[i] + arr[j]) == value)
                 count++;
-        }
-    }
     return count;
 }
+
 int countPairs2(int *arr, int len, int value) {
     int count = 0;
     int right = len - 1;
@@ -24,20 +23,35 @@ int countPairs2(int *arr, int len, int value) {
     }
     return count;
 }
+int binsearch(int *arr, int size, int value) {
+    int count = 0;
+    int left = 0;
+    int right = size - 1;
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+        if (arr[mid] == value) {
+            count++;
+            int leftPos = mid - 1;
+            while (leftPos >= 0 && arr[leftPos] == value) {
+                count++;
+                leftPos--;
+            }
+            int rightPos = mid + 1;
+            while (rightPos < size && arr[rightPos] == value) {
+                count++;
+                rightPos++;
+            }
+            return count;
+        } else if (arr[mid] < value) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+    return 0;
+}
 
 int countPairs3(int *arr, int len, int value) {
-    while (low <= high){
-        int mid = low + (high - low) / 2;
-        if (arr[mid] == key)
-            return mid;
-        else if (arr[mid] < key)
-            low = mid + 1;
-        else
-            high = mid - 1;
-    }
-    return -1;
-}
-int countPairs3(int *arr, int len, int value){
     int count = 0;
     for (int i = 0; i < len; i++) {
         int secondElem = value - arr[i];

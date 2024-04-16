b3ec957e4780f769e6e2058ff2aee8e790b35f25
PivovarovAndrei <160382197+PivovarovAndrei@users.noreply.github.com>
2024-04-16 21:00:57 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -11,24 +11,20 @@ int countPairs1(int *arr, int len, int value) {
 }
 int countPairs2(int *arr, int len, int value) {
     int count = 0;
-    int left = 0;
     int right = len - 1;
-    while (left < right){
-        int sum = arr[left] + arr[right];
-        if (sum == value){
-            count++;
-            left++;
-            right--;
-        }
-        else if (sum < value){
-            left++;
-        }
-        else{
-            right--;
+    while (arr[right] > value) {
+        right = right - 1;
+    }
+    for (int i = 0; i < right; i++) {
+        for (int j = right; j > i; j--) {
+            if ((arr[i] + arr[j]) == value) {
+                count++;
+            }
         }
     }
     return count;
 }
+
 int countPairs3(int *arr, int len, int value) {
     while (low <= high){
         int mid = low + (high - low) / 2;
@@ -43,11 +39,9 @@ int countPairs3(int *arr, int len, int value) {
 }
 int countPairs3(int *arr, int len, int value){
     int count = 0;
-    for (int i = 0; i < len; i++){
-        int complement = value - arr[i];
-        int index = binarySearch(arr, i + 1, len - 1, complement);
-        if (index != -1)
-            count++;
+    for (int i = 0; i < len; i++) {
+        int secondElem = value - arr[i];
+        count = count + binsearch(&arr[i + 1], len - i - 1, secondElem);
     }
     return count;
 }
